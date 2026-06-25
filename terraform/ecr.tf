fe161de8cb7bf7f4e5bae5120906370511a8eade
Ez <ezequiel2014.edlv@gmail.com>
2026-06-25 11:04:31 -0300

    "chore(secops): add checkov risk acceptance tags to ecr and create gh secret for AWS ARN ROLE"

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,4 +1,6 @@
 resource "aws_ecr_repository" "portfolio_repo" {
+  # checkov:skip=CKV_AWS_136: Usamos cifrado AES-256 por defecto de AWS (más barato y suficiente)
+  # checkov:skip=CKV_AWS_51: Habilitamos mutabilidad para entornos de desarrollo/stage dinámicos
   name                 = "portfolio-sre"
   image_tag_mutability = "MUTABLE"
 
