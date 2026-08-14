ccb5eb97bfaf76b02763098f63607eff1a2c45cd
Ez <ezequiel2014.edlv@gmail.com>
2026-08-13 23:25:03 -0300

    fix(iam): update GitHub Actions role policy to allow wildcard repository names

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -26,7 +26,7 @@ resource "aws_iam_role" "github_actions_role" {
           }
           StringLike = {
             "token.actions.githubusercontent.com:sub" = [
-              "repo:ezequieldlv/mysstic-cloud:ref:refs/heads/main",
+              "repo:ezequieldlv*/mysstic-cloud*:ref:refs/heads/main",
               "repo:ezequieldlv/portfolio-sre:ref:refs/heads/main"
             ]
           }
